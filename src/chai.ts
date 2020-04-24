e4b735ab2b4ad9faa610bd74677b571a005ed09a
Thodoris Greasidis <thgreasi@gmail.com>
2020-04-25 01:53:58 +0300

    Initial code commit
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

---
+++ src/chai.ts
@@ -0,0 +1,5 @@
+import { use } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+use(chaiAsPromised);
+
+export { expect } from 'chai';
---
+++ src/supertest.ts
@@ -0,0 +1,20 @@
+import type * as express from 'express';
+import * as supertest from 'supertest';
+import { UserParam } from './common';
+
+export default function (app: express.Express, user?: UserParam) {
+	// Can be an object with `token`, a JWT string or an API key string
+	let token = user;
+	if (typeof user === 'object' && user.token) {
+		token = user.token;
+	}
+	// We have to cast `as any` because the types are poorly maintained
+	// and don't support setting defaults
+	const req: any = supertest.agent(app);
+	req.set('X-Forwarded-Proto', 'https');
+
+	if (typeof token === 'string') {
+		req.set('Authorization', `Bearer ${token}`);
+	}
+	return req as ReturnType<typeof supertest.agent>;
+}
