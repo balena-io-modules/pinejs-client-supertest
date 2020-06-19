9f7589f3287edcf6d73dba9ada6ee04e09c4b623
Thodoris Greasidis <thgreasi@gmail.com>
2020-06-19 15:32:37 +0300

    Add method overrides to provide supertest typings in the results
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ type supportedMethod = 'get' | 'put' | 'patch' | 'post' | 'delete';
 
 // Use `T = any` by default, just like supertest does, but also support specifying
 // the expected type, so that we can avoid the ugly `() as Something` castings.
-type PromiseResult<T = any> = Omit<Test, 'expect' | 'then'> &
+type PromiseResult<T = any> = Promise<T> &
+	Omit<Test, 'expect' | 'then'> &
 	Promise<Overwrite<Response, { body: T }>> & {
 		// Atm we can't override just the return type of a method w/o re-defining all its variants.
 		expect(
@@ -60,11 +61,43 @@ export class PineTest extends PinejsClientCore<PineTest> {
 			expect(() => {
 				response.body = this.transformGetResult(normalizedParams)(body);
 			}).to.not.throw();
-		});
+		}) as PromiseResult<T>;
 	}
 
-	public post<T = any>(params: Params): PromiseResult<T> {
-		return super.post(params);
+	public put(
+		...args: Parameters<PinejsClientCore<PineTest>['put']>
+	): PromiseResult<{}> {
+		return super.put(...args) as PromiseResult<{}>;
+	}
+
+	public patch(
+		...args: Parameters<PinejsClientCore<PineTest>['patch']>
+	): PromiseResult<{}> {
+		return super.patch(...args) as PromiseResult<{}>;
+	}
+
+	public post(
+		...args: Parameters<PinejsClientCore<PineTest>['post']>
+	): PromiseResult<{}> {
+		return super.post(...args) as PromiseResult<{}>;
+	}
+
+	public delete(
+		...args: Parameters<PinejsClientCore<PineTest>['delete']>
+	): PromiseResult<{}> {
+		return super.delete(...args) as PromiseResult<{}>;
+	}
+
+	public upsert(
+		...args: Parameters<PinejsClientCore<PineTest>['upsert']>
+	): PromiseResult<{} | undefined> {
+		return super.upsert(...args) as PromiseResult<{} | undefined>;
+	}
+
+	public request(
+		...args: Parameters<PinejsClientCore<PineTest>['request']>
+	): PromiseResult<{}> {
+		return super.request(...args) as PromiseResult<{}>;
 	}
 
 	_request({
